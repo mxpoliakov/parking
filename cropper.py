24e5db33b299bf5328192b26c46c9398ff8438e1
User <mpoliakov11@gmail.com>
2018-12-12 16:04:52 +0200

    Model

--- cropper.py
+++ cropper.py
@@ -4,25 +4,25 @@ import os
 import numpy as np
 import cv2
 
-mapping = json.loads(open('mapping.json').read())[0]['annotations']
+def read_mapping(mapping_json):
+    mapping = json.loads(open(mapping_json).read())[0]['annotations']
+    points = []
 
-points = []
+    for parklot in mapping:
+        x = map(float, parklot['xn'].split(';'))
+        y = map(float, parklot['yn'].split(';'))
+        coords = np.array(list(zip(x, y))).astype(np.float32)
+        reorder = np.zeros((4, 2), dtype=np.float32)
+        reorder[0] = coords[np.argmin(coords.sum(axis=1))]
+        reorder[1] = coords[np.argmin(np.diff(coords, axis=1))]
+        reorder[2] = coords[np.argmax(coords.sum(axis=1))]
+        reorder[3] = coords[np.argmax(np.diff(coords, axis=1))]
+        points.append(reorder)
 
-for parklot in mapping:
-    x = map(float, parklot['xn'].split(';'))
-    y = map(float, parklot['yn'].split(';'))
-    coords = np.array(list(zip(x, y))).astype(np.float32)
-    reorder = np.zeros((4, 2), dtype=np.float32)
-    reorder[0] = coords[np.argmin(coords.sum(axis=1))]
-    reorder[1] = coords[np.argmin(np.diff(coords, axis=1))]
-    reorder[2] = coords[np.argmax(coords.sum(axis=1))]
-    reorder[3] = coords[np.argmax(np.diff(coords, axis=1))]
-    points.append(reorder)
+    return points
 
-w = 135
-h = 235
 
-for filename in glob.glob('images/*'):
+def crop(points, filename, out, w=135, h=235):
     img = cv2.imread(filename)
     for i, parklot in enumerate(points):
         parklot_rect = np.array([[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]], dtype=np.float32)
@@ -34,4 +34,5 @@ for filename in glob.glob('images/*'):
 
         cropped = cv2.warpPerspective(img, matrix, (w, h))
         base = os.path.splitext(os.path.basename(filename))[0]
-        cv2.imwrite('parklots/' + base + '+' + str(i) + '.jpg', cropped)
+
+        cv2.imwrite(out + '/' + base + '+' + str(i) + '.jpg', cropped)
---
+++ model.py
@@ -0,0 +1,40 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping 
+
+def train(input, out, rows=235, cols=135, epochs=25):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(rows, cols, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dense(units=1, activation='sigmoid'))
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+    train_generator = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+    test_generator = ImageDataGenerator(rescale=1./255)
+
+    train = train_generator.flow_from_directory(input + '/train', target_size=(rows, cols), class_mode='binary')
+    test = test_generator.flow_from_directory(input + '/test', target_size=(rows, cols), class_mode='binary')
+    callbacks = [EarlyStopping(monitor='val_loss', patience=2)]
+
+    model.fit_generator(train, epochs=epochs, 
+                    steps_per_epoch=train.samples // 32, 
+                    validation_data=test, validation_steps=test.samples // 32,
+                    class_weight={0:1, 1:3}, callbacks=callbacks)
+    
+    validation = test_generator.flow_from_directory(input + '/validation', target_size=(rows, cols), class_mode='binary')
+
+    score = model.evaluate_generator(validation, validation.samples // 32, workers=12)
+    print('Validation accuracy:', score[1])
+    model.save(out)
+    print('Model saved')
+
+def predict_one(model, img):
+    return model.predict_classes(np.expand_dims(img, axis=0) / 255)[0][0]
